d3764d165b08171595b593e446211c95ed468d73
Eugenia Fang <Eugenia@Eugenias-MacBook-Pro.local>
2013-08-20 13:55:00 -0400

    created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyTesting::Application.config.secret_key_base = '94462ef88aece1af850a85dad96d3f9eee776a4c0fff182653ca23d14b6a651868a00f16a262238d77fac5ec2631df5dbbef0e5d57693128dcd863a337d621fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyTesting::Application.config.session_store :cookie_store, key: '_Ruby_testing_session'
